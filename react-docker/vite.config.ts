3722728b9f2d75a8dd5c710f2145347c250e9c4c
Akash Butala <akash.butala.365@gmail.com>
2024-03-14 03:13:31 -0700

    Final commit

---
+++ react-docker/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // This is needed to reflect the code changes with docker
+    },
+  }
+})
